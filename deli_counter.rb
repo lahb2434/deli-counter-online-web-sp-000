c9519bbe54d8cde7359b8c180472163022f1275a
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-26 17:59:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(customer)
          new_customer.push("#{position}. #{name}")
     end
   new_customer
-    puts "The line is currently: #{new_customer.join" "}"
+    puts "The line is currently: #{new_customer.join(" ")}"
   end
 end
 
