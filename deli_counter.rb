9092e755082bd2d934165478b15004bd30f6ad49
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-26 18:20:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,6 +13,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
+  katz_deli.each_with_index{ |name, position| position += 1 ; puts "Welcome, #{name}. You are number #{position} in line."
 end
   
   
