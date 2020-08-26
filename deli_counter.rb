27a2a821f4b845c9d175703f6b19cb906791d98f
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-26 17:17:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 def line(customer)
+  new_customer = []
   if customer.length == 0 puts "The line is currently empty." 
 else
-  customer.each_with_index do |name, postion| position += 1 
+  customer.each_with_index do |name, postion| position += 1 ; new_customer.push('')
   puts 
 end
