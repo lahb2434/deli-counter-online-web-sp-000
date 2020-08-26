f6cae18379897d3eb745a318682ebd2c245b4510
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-26 18:52:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,9 +16,10 @@ def take_a_number(katz_deli, customer)
   puts "Welcome, #{katz_deli[-1]}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(customer)
-  if customer.length == 0 
+def now_serving(katz_deli, customer)
+  if katz_deli.length == 0 
       puts "There is nobody waiting to be served!"
+  else 
   end
 end
 
