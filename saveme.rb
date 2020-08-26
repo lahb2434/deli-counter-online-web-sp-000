6cbfd8ed8b7a09d40e958110603f260b72924308
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-26 18:21:42 +0000

    Automatically backed up by Learn

--- saveme.rb
+++ saveme.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  katz_deli.each_with_index{ |name, position| position += 1 ; puts "Welcome, #{name}. You are number #{position} in line."
+  katz_deli.each_with_index{ |name, position| position += 1 ; puts "Welcome, #{name}. You are number #{position} in line."}
 end
 
 take_a_number(katz_deli, "christin")
